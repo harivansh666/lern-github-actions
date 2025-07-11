30b879f0a9647787677f554723fced26fcfb3685
Harivansh <jazzz1815@gmail.com>
2025-07-11 21:43:25 +0530

    adding css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
+import "./App.css";
 const App = () => {
-  const heading = {
-    color: "red",
-    textAlign: "center",
-    fontSize: "2rem",
-    margin: "20px 0",
-    fontWeight: "bold", // Corrected this line
-    fontFamily: "Arial, sans-serif",
-  };
-
   return (
     <>
-      <h1 style={heading}>Hello, World!</h1>;
+      <h1 className="heading">Hello, World!</h1>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
         voluptatum magnam quidem doloribus facilis beatae enim? Ipsam excepturi
